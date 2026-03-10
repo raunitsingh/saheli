0f9608760b1adb8446996ea8261d7a3e32107654
raunitsingh <raunitsingh33@gmail.com>
2026-03-10 23:52:12 +0530

    sync from ubuntu

---
+++ backen/app/ai/test_interview.py
@@ -0,0 +1,13 @@
+from interview_engine import InterviewEngine
+
+engine = InterviewEngine()
+
+print(engine.start_interview("fever"))
+
+print(engine.next_question("3 days"))
+
+print(engine.next_question("yes chills"))
+
+print(engine.next_question("body pain"))
+
+print(engine.next_question("no vomiting"))
